f973ca1296af50f7a122db00709c9249a1fed8ca
HubRobloxScript <supornthanaphat@gmail.com>
2026-05-27 16:51:41 +0700

    fix: swap totem pop overlay texture to totem_of_cycle when our totem is held

--- src/client/java/com/fou/client/mixin/CycleTotemPopMixin.java
+++ src/client/java/com/fou/client/mixin/CycleTotemPopMixin.java
@@ -1,32 +1,41 @@
 package com.fou.client.mixin;
 
 import com.fou.registry.ModItems;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(ClientPlayNetworkHandler.class)
+@Mixin(InGameHud.class)
 public class CycleTotemPopMixin {
-	@Inject(method = "getActiveDeathProtector", at = @At("HEAD"), cancellable = true)
-	private static void fou$useCycleTotemPopItem(PlayerEntity player, CallbackInfoReturnable<ItemStack> cir) {
-		for (Hand hand : Hand.values()) {
-			if (player.getStackInHand(hand).contains(DataComponentTypes.DEATH_PROTECTION)) {
-				return;
-			}
-		}
 
-		for (Hand hand : Hand.values()) {
-			ItemStack stack = player.getStackInHand(hand);
-			if (stack.isOf(ModItems.INSTANCE.getTOTEM_OF_CYCLE())) {
-				cir.setReturnValue(stack);
-				return;
-			}
-		}
-	}
+    private static final Identifier CYCLE_TOTEM_TEXTURE =
+            Identifier.of("fou", "textures/item/totem_of_cycle.png");
+
+    @ModifyArg(
+        method = "renderMiscOverlays",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/hud/InGameHud;renderOverlay(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/util/Identifier;F)V"
+        ),
+        index = 1
+    )
+    private Identifier fou$swapTotemOverlayTexture(Identifier original) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null) {
+            for (Hand hand : Hand.values()) {
+                ItemStack stack = client.player.getStackInHand(hand);
+                if (stack.isOf(ModItems.INSTANCE.getTOTEM_OF_CYCLE())) {
+                    return CYCLE_TOTEM_TEXTURE;
+                }
+            }
+        }
+        return original;
+    }
 }
